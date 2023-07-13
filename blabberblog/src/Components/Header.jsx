fc2273b88f5590dbe6dba5da1b42e530d52c5527
sailakshmy <sailakshmy94@gmail.com>
2023-07-13 23:17:05 +0530

    Commit-3:- Added the code snippets for the Header section

--- blabberblog/src/Components/Header.jsx
+++ blabberblog/src/Components/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/Images/Logo.png';
+import banner from '../assets/Images/Banner.jpg';
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className='flex justify-between items-center'>
+      <img src={logo} className='w-[100px] cursor-pointer'/>
+      <ul className='flex gap-4 md:gap-14'>
+        <li className='hover:font-bold cursor-pointer'>Home</li>
+        <li className='hover:font-bold cursor-pointer'>About Us</li>
+        <li className='hover:font-bold cursor-pointer'>Contact Us</li>
+      </ul>
+      <button className='bg-red-500 rounded-full'>Sign In</button>
     </div>
   )
 }
