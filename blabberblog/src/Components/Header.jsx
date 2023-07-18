02afe9a5b4e5d31c3bb6307f784f95994467873e
sailakshmy <sailakshmy94@gmail.com>
2023-07-18 22:24:06 +0530

    Commit-4:- Added the code snippets for the Search section, with the banners and the different tabs, Modified the colors in the Header component to match the theme colors of the blog

--- blabberblog/src/Components/Header.jsx
+++ blabberblog/src/Components/Header.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import logo from '../assets/Images/Logo.png';
-import banner from '../assets/Images/Banner.jpg';
 
 function Header() {
   return (
     <div className='flex justify-between items-center'>
-      <img src={logo} className='w-[100px] cursor-pointer'/>
+      <img src={logo} className='w-[100px] cursor-pointer rounded-2xl'/>
       <ul className='flex gap-4 md:gap-14'>
         <li className='hover:font-bold cursor-pointer'>Home</li>
         <li className='hover:font-bold cursor-pointer'>About Us</li>
         <li className='hover:font-bold cursor-pointer'>Contact Us</li>
       </ul>
-      <button className='bg-red-500 rounded-full'>Sign In</button>
+      <button className='bg-blue-900 rounded-full'>Sign In</button>
     </div>
   )
 }
--- blabberblog/src/Components/Search.jsx
+++ blabberblog/src/Components/Search.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import banner from '../assets/Images/Banner.jpg';
+import { IoSearchOutline } from "react-icons/io5";
+
 
 function Search() {
+  const tags = [
+    {
+      id:1,
+      name:'All'
+    },
+    {
+      id:2,
+      name:'React'
+    },
+    {
+      id:3,
+      name:'JS'
+    },
+    {
+      id:4,
+      name:'Vue'
+    },
+  ];
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
-    <div>
-      Search
+    <div className='flex flex-col justify-center mt-8 px-[100px] md:px-[200px]'>
+      <img src={banner} className='rounded-2xl'/>
+      <div className='bg-[#3b3b3b] rounded-lg p-3 mt-[-30px] mx-[25%] flex items-center'>
+        <IoSearchOutline className='text-[20px] text-gray-400'/>
+        <input type='text' placeholder='Search' className='outline-none ml-2'/>
+      </div>
+      <div className='flex gap-10 justify-center mt-5'>
+          {tags.map((tag,index)=>(
+            <ul onClick={()=>setActiveIndex(index)}
+                key={tag.id}
+                className={`${index === activeIndex?'bg-blue-900':null}
+                  p-2 pb-3 rounded-sm md:rounded-full cursor-pointer md:px-4
+                  hover:scale-110 hover:border-[1px] border-blue-900
+                  transition-all duration-100 ease-in-out`
+                }
+            >
+              <li>{tag.name}</li>
+            </ul>
+          ))}     
+      </div>
     </div>
   )
 }
